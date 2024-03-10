a0793420f5841d9b91fd2e71367634c7851f2204
codzk <zekemilne@gmail.com>
2024-03-10 20:26:16 +1000

    add register and login

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     name = db.Column(db.String)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean, default=True)
 
 class UserSchema(ma.Schema):
     class Meta:
