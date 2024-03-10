6aca4f4746e332862c691d81643be21d82e40170
codzk <zekemilne@gmail.com>
2024-03-10 18:35:28 +1000

    Create Cli Controller

--- models/user.py
+++ models/user.py
@@ -4,17 +4,17 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'username', 'password', 'is_admin')
+        fields = ('id', 'name', 'email', 'password', 'is_admin')
 
     
 user_schema = UserSchema(exclude=['password'])
-users_schema = UserSchema(exclude=['password'])
+# users_schema = UserSchema(exclude=['password'])
 
 
 
